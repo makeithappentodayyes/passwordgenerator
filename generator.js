2c4ecafba286f4131041fc0627161f40d5b169a3
Daniel Gil <danielgilucsd@gmail.com>
2019-10-10 17:25:29 -0700

    Fixed Prompts on password generator

--- generator.js
+++ generator.js
@@ -8,37 +8,32 @@
 
  var upperCase = confirm("Uppercase Characters")
 
-function generate_password(){            
-var userinput = document.getElementById("password") .value
-var validlenght = false 
-console.log(userinput)
-if (userinput.lenght>8 && userinput.lenght<125)
-{
-
-    console.log("validlenght")
-}
-
-
-function generate(){
-
-    
-    let complexity = document.getElementById("slider").value;
-    
-
-    let values = "ABCDEFHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
-    }
+ 
     
     
-    let password = "";
+ 
+ 
+ 
     
     
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.lenght - 1)));
+    function generate(){
+        
     
-    }
-    
-    
-    
-    
-    document.getElementById("display").value = password;
+        let values = "ABCDEFHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
+        }
+        
+        
+        let password = "";
+        
+        
+        for(var i = 0; i <= complexity; i++){
+            password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.lenght - 1)));
+        
+        }
+        
+        
+        
+        
+        document.getElementById("display").value = password;
+        
     
